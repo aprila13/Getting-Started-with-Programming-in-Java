bc967a2d2ed659a68701726674fda208488c3c07
aprila13.name <aprilacraig1@gmail.com>
2021-04-13 15:01:40 -0400

    Initial commit

---
+++ src/com/April/Main.java
@@ -0,0 +1,13 @@
+package com.April;
+
+public class Main {
+
+    //the main method is the entry point for the application
+    public static void main(String[] args) {
+
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+
+    }
+}
